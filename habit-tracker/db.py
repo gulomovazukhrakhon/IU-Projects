7a30ed954ce09226fa873fd11f0c7260870996f9
Zukhrakhon Gulomova <110628098+gulomovazukhrakhon@users.noreply.github.com>
2023-01-19 22:41:59 +0500

    Update db.py

--- habit-tracker/db.py
+++ habit-tracker/db.py
@@ -11,19 +11,18 @@ def get_db(name):
 def create_tables(db):
     cur = db.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS habit_data ("
-                "name varchar(250) NOT NULL, frequency text, "
-                "periodicity varchar(250) NOT NULL, unitset varchar(250) NOT NULL, "
-                "start_date varchar(250) NOT NULL, end_date varchar(250) NOT NULL, check_off INTEGER,"
-                "last_updated_day varchar(250) NOT NULL, streak_days INTEGER)")
+                "name varchar(250) NOT NULL, frequency varchar(250) NOT NULL, "
+                "periodicity varchar(250) NOT NULL, start_date varchar(250) NOT NULL, check_off INTEGER,"
+                "last_updated_day varchar(250) NOT NULL, streak_days INTEGER, longest_st_days INTEGER)")
     db.commit()
 
 
-def add_habits(db, name: str, frequency: str, periodicity: str, unitset: str, start_date: str,
-               end_date: str, check_off: int, last_updated_day: str, streak_days: int):
+def add_habits(db, name: str, frequency: str, periodicity: str, start_date: str,
+               check_off: int, last_updated_day: str, streak_days: int, longest_st_days: int):
     cur = db.cursor()
-    cur.execute(f"INSERT INTO habit_data VALUES('{name}', '{frequency}', "
-                f"'{periodicity}', '{unitset}', '{start_date}', '{end_date}', "
-                f"'{check_off}', '{last_updated_day}', '{streak_days}')")
+    cur.execute(f"INSERT INTO habit_data VALUES("
+                f"'{name}', '{frequency}', '{periodicity}', '{start_date}', "
+                f"'{check_off}', '{last_updated_day}', '{streak_days}', '{longest_st_days}')")
     db.commit()
 
 
@@ -39,8 +38,8 @@ def get_rows(db, thing, value):
 
     def pretty_table(list_rows):
         p_table = PrettyTable()
-        p_table.field_names = ['Name', 'Frequency', 'Periodicity', 'Unitset', 'Start Date', 'End Date',
-                               'Check-off', 'Last Updated Day', 'Streak Days']
+        p_table.field_names = ['Name', 'Frequency', 'Periodicity', 'Start Date',
+                               'Check-off', 'Last Updated Day', 'Streak Days', 'Longest St. Days']
         rows_listed = [list(row) for row in list_rows]
         p_table.add_rows(rows_listed)
         p_table.set_style(ORGMODE)
@@ -48,3 +47,4 @@ def get_rows(db, thing, value):
 
     table = pretty_table(list_rows=rows)
     return table
+
