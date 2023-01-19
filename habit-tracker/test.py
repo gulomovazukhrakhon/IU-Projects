68a92370d84469c1abac6c6037ba3da97c988b2b
Zukhrakhon Gulomova <110628098+gulomovazukhrakhon@users.noreply.github.com>
2023-01-19 22:44:53 +0500

    Update test.py

--- habit-tracker/test.py
+++ habit-tracker/test.py
@@ -4,22 +4,18 @@ from habit import *
 class TestHabitTracking:
 
     def test_add_habit(self):
-        create_habit = CreateHabit(name="reading", frequency="daily", periodicity="4 months",
-                                   unitset="40 minutes", start_date="20/12/2022")
+        create_habit = CreateHabit(name="coding", frequency="daily", periodicity="15 months")
 
-        create_habit.add_habit(db_file="test.db")
+        create_habit.add_habit(db_file="predefined-habits.db")
 
     def test_manage_habit(self):
         manage_habit = ManageHabit()
-        manage_habit.edit(name="workout", thing="unitset", value="30 minutes", db_file="test.db")
-        manage_habit.check(name="workout", db_file="test.db")
-        manage_habit.report_all(db_file="test.db")
-        manage_habit.report_by_frequency(frequency='daily', db_file="test.db")
-        manage_habit.report_one(name="workout", db_file="test.db")
+        manage_habit.edit(name="workout", thing="periodicity", value="6 months", db_file="predefined-habits.db")
+        manage_habit.check(name="workout", db_file="predefined-habits.db")
+        manage_habit.report_all(db_file="predefined-habits.db")
+        manage_habit.report_by_frequency(frequency='daily', db_file="predefined-habits.db")
+        manage_habit.report_one(name="workout", db_file="predefined-habits.db")
 
     def test_delete_habit(self):
-        delete_habit = DeleteHabit(habit_name="coding")
-        delete_habit.delete_habit_manually(db_file="test.db")
-        delete_habit.delete(db_file="test.db")
-        # import os
-        # os.remove('test.db')
+        delete_habit = DeleteHabit(habit_name="walking")
+        delete_habit.delete(db_file="predefined-habits.db")
