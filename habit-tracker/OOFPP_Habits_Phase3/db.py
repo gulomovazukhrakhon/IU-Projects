04548bd63424452ccc2b3a5622f06521808afb52
Zukhrakhon Gulomova <110628098+gulomovazukhrakhon@users.noreply.github.com>
2023-02-13 19:43:07 +0500

    Update db.py

--- habit-tracker/OOFPP_Habits_Phase3/db.py
+++ habit-tracker/OOFPP_Habits_Phase3/db.py
@@ -3,12 +3,20 @@ from prettytable import *
 
 
 def get_db(name):
+    """
+    Opens the db file.
+        :param name: the name of the db file
+    """
     db = sqlite3.connect(name)
     create_tables(db)
     return db
 
 
 def create_tables(db):
+    """
+    Creates tables in the database file.
+        :param db: the name of the db file
+    """
     cur = db.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS habit_data ("
                 "name varchar(250) NOT NULL, frequency varchar(250) NOT NULL, "
@@ -19,6 +27,18 @@ def create_tables(db):
 
 def add_habits(db, name: str, frequency: str, periodicity: str, start_date: str,
                check_off: int, last_updated_day: str, streak_days: int, longest_st_days: int):
+    """
+    Stores habit data in a database file.
+        :param db: the name of the db file
+        :param name: the name of the habit
+        :param frequency: the frequency of the habit
+        :param start_date: the date when this habit was created
+        :param periodicity: how long the habit lasts
+        :param check_off: how many times the habit was performed
+        :param last_updated_day: the day when the habit was marked as completed
+        :param streak_days: the number of streak days
+        :param longest_st_days: the number of the longest streak days
+    """
     cur = db.cursor()
     cur.execute(f"INSERT INTO habit_data VALUES("
                 f"'{name}', '{frequency}', '{periodicity}', '{start_date}', "
@@ -27,6 +47,12 @@ def add_habits(db, name: str, frequency: str, periodicity: str, start_date: str,
 
 
 def get_rows(db, thing, value):
+    """
+    Visualization of habit data in a table.
+        :param db: the name of the db file
+        :param thing: the name of the column in the table
+        :param value: the value of the column
+    """
     cur = db.cursor()
 
     if thing is None and value is None:
@@ -47,4 +73,3 @@ def get_rows(db, thing, value):
 
     table = pretty_table(list_rows=rows)
     return table
-
